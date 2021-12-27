3d98525811fb48b584335025af8c1a6259c6f1ca
Raghav Gupta <raghav2008gupta@gmail.com>
2021-12-27 02:03:53 -0600

    feat: complete practice 03_07

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,37 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const country = {
+    name: "USA",
+    noOfStates: 50,
+    state: [
+        {
+            name: "Arkansas",
+            noOfCounties: 20,
+            updateNoOfCounties: function (newNoOfCounties) {
+                this.noOfCounties = newNoOfCounties;
+            },
+            city: {
+                name: "Fayetteville"
+            }
+        },
+        {
+            name: "California",
+            noOfCounties: 20,
+            city: {
+                name: "San Francisco"
+            }
+        },
+        {
+            name: "Florida",
+            noOfCounties: 20,
+            city: {
+                name: "Orlando"
+            }
+        }
+    ]
+};
+
+console.log("This is %s city.", country.state[0].city.name);
+console.log("%s has %d counties.", country.state[0].name, country.state[0].noOfCounties);
